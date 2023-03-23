9e372c1c09cc1b187d49a6aa9c9dc4516d39100e
PaulHarrer <mail@paulharrer.at>
2023-03-23 21:21:23 +0100

    Feature: Added first implementation

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # apifonica-client
-Unofficial Python Package to easily integrate the apifonica API to your project
+Unofficial Python Package to easily integrate the [Apifonica](https://www.apifonica.com/) API to your project.
+
+> Note: Currently this only supportings sending SMS messages. Other api endpoints will be implemented over time.
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ApifonicaClient",
+    version="1.0.0",
+    description="Unofficial Python Package to easily integrate the Apifonica API to your project",
+    author="Paul Harrer",
+    author_email="mail@paulharrer.at",
+    packages=find_packages(),
+    install_requires=[
+        "requests"
+    ],
+    project_urls={
+        "GitHub": "https://github.com/PaulHarrer/apifonica-client",
+    },
+)
