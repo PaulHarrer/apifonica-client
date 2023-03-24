acd7e67ff4f7211999f89d94c487c833f4fdaf04
PaulHarrer <mail@paulharrer.at>
2023-03-24 21:00:40 +0100

    Patch: added long description for pypi and increased version number

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="ApifonicaClient",
-    version="1.0.0",
+    version="1.0.1",
     description="Unofficial Python Package to easily integrate the Apifonica API to your project",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author="Paul Harrer",
     author_email="mail@paulharrer.at",
     packages=find_packages(),
